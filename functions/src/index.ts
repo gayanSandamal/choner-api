a340629b3c7d9fb122f0de3cc92ecbc57735e099
Gayan Sandamal <gayansandamal94@gmail.com>
2024-09-29 23:38:15 +0530

    publish scheduled interest posts job

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { createUserDocument, getUser, setUser, deleteUser } from "./user";
 import { sendEmail } from "./email";
-import { createInterest } from "./interest";
+import { createInterest, publishScheduledInterestsJob } from "./interest";
 
-export { createUserDocument, getUser, setUser, deleteUser, sendEmail, createInterest };
+export { createUserDocument, getUser, setUser, deleteUser, sendEmail, createInterest, publishScheduledInterestsJob };
